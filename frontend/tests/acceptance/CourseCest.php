fbee9dc23d409ff89b091ec2e63e3073ef33da7f
JoaoBacano <91315939+JoaoBacano@users.noreply.github.com>
2024-01-08 10:34:48 +0000

    acceptance test

--- frontend/tests/acceptance/CourseCest.php
+++ frontend/tests/acceptance/CourseCest.php
@@ -5,47 +5,38 @@ use common\models\Course;
 use common\models\User;
 use frontend\tests\AcceptanceTester;
 use MongoDB\Driver\Cursor;
+use Codeception\Module\WebDriver;
 
 class CourseCest
 {
-    public function _fixtures()
-    {
-        return [
-            'user' => [
-                'class' => UserFixture::class,
-                'dataFile' => codecept_data_dir() . 'user.php'
-            ]
-        ];
-    }
-
-    public function _before(AcceptanceTester $I)
-    {
-        $I->seeRecord(User::className(), ['username'=>'Instrutor']);
-        $I->amLoggedInAs(User::findOne(['username' => 'Instrutor']));
-    }
-
     public function tryToCreateCourse(AcceptanceTester $I)
     {
+        $I->amOnPage('site/login');
+        $I->wait(3);
+        $I->fillField('Username', 'Instrutor');
+        $I->fillField('Password', '12345678');
+        $I->click('Login');
+        $I->wait(3);
         $I->amOnPage('/course/create'); // Rota para a página de criação de curso
-
+        $I->wait(3);
         $I->see('Create Course'); // Verifica se está na página de criação de curso
 
         $I->fillField('Title', 'Teste Curso'); // Preenche o campo de título do curso
         $I->fillField('Description', 'Descrição do meu curso'); // Preenche o campo de descrição do curso
         $I->fillField('Price', '30');
         $I->selectOption('Skill Level', '1');
-        $I->attachFile('imageFile', 'curso.jpg'); // Anexa uma imagem para o curso
-        $I->selectOption('Category', 'Design');
+        $I->attachFile('File', 'curso.jpg'); // Anexa uma imagem para o curso
+        $I->selectOption('Category', 'teste1');
         $I->click('Save'); // Submete o formulário
-
-        $I->seeRecord(Course::className(), ['nome' => 'Curso']);
+        $I->wait(3);
 
         $I->see('Create Lesson'); // Verifica se está na página de criação de aula
-
+        $I->wait(3);
         $I->click('Create Section');
         $I->fillField('Title', 'Section Teste'); // Preenche o campo de título da aula
         $I->fillField('Order', '1'); // Preenche o campo de descrição da aula
         $I->click('Save'); // Submete o formulário
+        $I->wait(3);
 
         $I->click('Create Quiz');
         $I->fillField('Title', 'Quiz Teste'); // Preenche o campo de título da aula
@@ -54,29 +45,34 @@ class CourseCest
         $I->fillField('Number Questions', '10');
         $I->fillField('Max Points', '10');
         $I->click('Save'); // Submete o formulário
+        $I->wait(3);
 
         $I->see('Create Question');
-        $I->fillField('Title', 'Question Teste');
+        $I->fillField('Text', 'Question Teste');
         $I->fillField('Option One', 'Teste Question');
         $I->fillField('Option Two', 'Teste Question');
         $I->fillField('Option Three', 'Teste Question');
         $I->fillField('Option Four', 'Teste Question');
         $I->selectOption('Correct Answer', 'Option 1');
         $I->click('Save');
+        $I->wait(3);
 
         $I->see('Create Lesson'); // Verifica se está na página de criação de aula
         $I->fillField('Title', 'Teste Aula'); // Preenche o campo de título da aula
         $I->fillField('Context', 'Descrição da minha aula'); // Preenche o campo de descrição da aula
         $I->selectOption('Section', 'Section Teste'); // Seleciona a seção criada anteriormente
-        $I->selectOption('Lesson Type', 'Curso'); // Seleciona o curso criado anteriormente
+        $I->selectOption('Lesson Type', 'video'); // Seleciona o curso criado anteriormente
         $I->attachFile('File', 'curso.jpg'); // Anexa um vídeo para a aula
         $I->selectOption('Quiz', 'Quiz Teste');
         $I->click('Save'); // Submete o formulário
+        $I->wait(3);
 
-        $I->click('MY COURSES');
+        $I->amOnPage('/course/mycourse'); // Rota para a página de cursos
         $I->see('Courses');
-        $I->click('.bi-x','.float-end','Teste Aula'); // Apaga o curso
+        $I->click('.bi-x','.float-end','Teste Curso'); // Apaga o curso
+        $I->wait(3);
         $I->acceptPopup();
+        $I->wait(3);
     }
 
 
